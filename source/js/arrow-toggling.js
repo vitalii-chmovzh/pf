057c5423761d123ed4c0179313effe949a83882d
Karnaukhov-kh <karnaukhov.kh@gmail.com>
2015-12-11 17:55:40 +0200

    Fixes

--- source/js/arrow-toggling.js
+++ source/js/arrow-toggling.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
 	$(".sort-actions__price").on("click touchstart", function(){
-		$(".arrow-bottom").addClass("arrow-rotated-full");
+		$(".arrow-up").toggleClass("arrow-down");
 		console.log(1);
 	});
+	
+	
+  var noveltyBtn = $(".sort-actions__novelty"),
+      priceBtn = $(".sort-actions__price"),
+	  allBtns = $(".sort-actions__row span");
+	
+  noveltyBtn.addClass("toggler-active");
+  
+	allBtns.on("click touchstart", function(){
+		allBtns.removeClass("toggler-active");
+		$(this).addClass("toggler-active");
+	});
+	
+	noveltyBtn.on("click touchstart", function(){		
+		$(".arrow-up").removeClass("arrow-down");
+	});
 });
+
