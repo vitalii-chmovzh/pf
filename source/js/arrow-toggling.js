548e5d45efb75d58ddd62d652e19206ca7f8da9b
Vitaliy Korchak <samgold172@gmail.com>
2016-02-09 14:24:46 +0200

    fixes

--- source/js/arrow-toggling.js
+++ source/js/arrow-toggling.js
@@ -1,24 +1,21 @@
 $(document).ready(function(){
 
-	$(".sort-actions__price").on("click", function(){
-		$(".arrow-up").toggleClass("arrow-down-active");
-		console.log(1);
-	});
-
-
   var noveltyBtn = $(".sort-actions__novelty"),
       priceBtn = $(".sort-actions__price"),
-	  allBtns = $(".sort-actions__row span");
+	  allBtns = $(".sort-actions__row a");
 
   noveltyBtn.addClass("toggler-active");
+
+	priceBtn.on("click", function(){
+		if($(this).hasClass("toggler-active")){
+			$(".arrow-up").toggleClass("arrow-down-active");
+		}
+	});
 
 	allBtns.on("click", function(){
 		allBtns.removeClass("toggler-active");
 		$(this).addClass("toggler-active");
 	});
 
-	noveltyBtn.on("click", function(){
-		$(".arrow-up").removeClass("arrow-down-active");
-	});
 });
 
